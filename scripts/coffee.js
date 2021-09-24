e7e4d09f7b0d9571c2bbabce03c16a1227d4e581
khyati-l <khyati.lingerker@education.nsw.gov.au>
2021-09-24 15:11:06 +1000

    finalising

---
+++ scripts/coffee.js
@@ -0,0 +1,22 @@
+function displayFlatWhite() {
+      document.getElementById("flatWhitePanelID").style.opacity = "85%";
+}
+function flatWhiteCloseWindow() {
+    document.getElementById("flatWhitePanelID").style.opacity = "1%";
+}
+
+
+function displayLongBlack() {
+  document.getElementById("longBlackPanelID").style.opacity = "85%";
+}
+function longBlackCloseWindow() {
+document.getElementById("longBlackPanelID").style.opacity = "1%";
+}
+
+
+function displayLatte() {
+  document.getElementById("lattePanelID").style.opacity = "85%";
+}
+function latteCloseWindow() {
+document.getElementById("lattePanelID").style.opacity = "1%";
+}
---
+++ scripts/pattisserie.js
@@ -0,0 +1,22 @@
+function displayMuffin() {
+    document.getElementById("muffinPanelID").style.opacity = "85%";
+}
+function muffinCloseWindow() {
+  document.getElementById("muffinPanelID").style.opacity = "1%";
+}
+
+
+function displayCake() {
+document.getElementById("cakePanelID").style.opacity = "85%";
+}
+function cakeCloseWindow() {
+document.getElementById("cakePanelID").style.opacity = "1%";
+}
+
+
+function displayPie() {
+document.getElementById("piePanelID").style.opacity = "85%";
+}
+function pieCloseWindow() {
+document.getElementById("piePanelID").style.opacity = "1%";
+}
